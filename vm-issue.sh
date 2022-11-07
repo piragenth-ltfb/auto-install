6635b171bd4080a8997ebee041a881fb8ce405ce
piragenth-ltfb <linuxtutorialforbeginners@gmail.com>
2022-11-07 21:12:58 +0530

    piragenth

---
+++ vm-issue.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo 3559 | sudo -S vmware-modconfig --console --install-all
---
+++ vm-issues-and-more.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#echo 3559 | sudo -S vmware-modconfig --console --install-all
+
+
+#echo 3559 | sudo -S mount /dev/sdb /home/piragenth/drive-500GB
+echo 3559 | sudo -S vmware-modconfig --console --install-all
+xset led
