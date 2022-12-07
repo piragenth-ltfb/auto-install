014c3aa33381eb04d0bda7406bb325dee8af1ed9
piragenth-ltfb <linuxtutorialforbeginners@gmail.com>
2022-12-07 14:10:57 +0000

    sdifjso

--- installing/terminal-install.sh
+++ installing/terminal-install.sh
@@ -4,7 +4,29 @@ bash pkg-install.sh
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo '''if [ -x "$(command -v colorls)" ]; then
+echo '''
+
+if [ -x "$(command -v colorls)" ]; then
+    alias ls="colorls"
+    alias la="colorls -g -al"
+fi
+
+export PATH=/home/piragenth/.local/bin:$PATH
+alias cat='bat'
+alias vim='lvim'
+alias rm='rip'
+alias cp='xcp'
+alias grep='ripgrep'
+alias find='fd'
+alias ps='procs'
+alias tree='broot'
+alias du='dust'
+alias top='btm'
+alias web='web_search'
+eval "$(zoxide init zsh)"
+alias cd='z'
+
+if [ -x "$(command -v colorls)" ]; then
     alias ls="colorls"
     alias la="colorls -al"
 fi
@@ -12,4 +34,3 @@ fi
 
 LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 
-#alias ing 
