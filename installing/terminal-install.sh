9e2f5a47963ffc39a27dc64d2956a2580af90042
piragenth-ltfb <linuxtutorialforbeginners@gmail.com>
2022-12-12 22:02:52 +0530

    osfdoi

--- installing/terminal-install.sh
+++ installing/terminal-install.sh
@@ -1,16 +1,23 @@
 #!/bin/bash -i
 
-bash pkg-install.sh
+#bash pkg-install.sh
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo '''
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+#mkdir ~/.npm-global
+#npm config set prefix '~/.npm-global'
+#echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.profile
+#source ~/.profile
+#npm install -g jshint
 
-if [ -x "$(command -v colorls)" ]; then
-    alias ls="colorls"
-    alias la="colorls -g -al"
-fi
+#cp ~/.zshrc ~/.zshrc.bakup
 
+sed -i 's|plugins=(git)|
+plugins=(git web-search zsh-syntax-highlighting zsh-autosuggestions)
+export PATH="/home/$USER/.local/share/gem/ruby/3.0.0/bin:$PATH"
 export PATH=/home/piragenth/.local/bin:$PATH
 alias cat='bat'
 alias vim='lvim'
@@ -23,14 +30,14 @@ alias tree='broot'
 alias du='dust'
 alias top='btm'
 alias web='web_search'
-eval "$(zoxide init zsh)"
+eval '$(zoxide init zsh)'
 alias cd='z'
 
 if [ -x "$(command -v colorls)" ]; then
     alias ls="colorls"
     alias la="colorls -al"
 fi
-''' >> ~/.zshrc
+' ~/.zshrc
 
-LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+#LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 
